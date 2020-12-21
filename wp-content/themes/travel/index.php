2c0e667e1eaf632f8bea8f2437271b1eb3e13e90
isrt09 <mazedur@gmail.com>
2020-12-21 21:20:57 +0600

    theme details

---
+++ wp-content/themes/travel/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset') ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Travel</title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
+<body>
+	<h1><?php bloginfo('title') ?></h1>
+</body>
+</html>
