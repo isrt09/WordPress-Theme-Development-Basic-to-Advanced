21fee0a5bfe9f02e3a283056446147adbec212e4
isrt09 <mazedur@gmail.com>
2020-12-22 00:11:33 +0600

    Register Sidebar

--- wp-content/themes/travel/functions.php
+++ wp-content/themes/travel/functions.php
@@ -6,4 +6,20 @@
 	add_theme_support('post-thumbnails');
 	add_theme_support('post-formats',['video','audio','gallery']);
 	add_theme_support('custom-logo');
+
+	register_sidebar([
+		'name'        => 'LEFT SIDEBAR',
+		'description' => 'This is for Left Sidebar Area Section',
+		'id'          => 'left-sidebar'
+	]);	
+	register_sidebar([
+		'name'        => 'RIGHT SIDEBAR',
+		'description' => 'This is for Right Sidebar Area Section',
+		'id'          => 'right-sidebar'
+	]);	
+	register_sidebar([
+		'name'        => 'FOOTER SIDEBAR',
+		'description' => 'This is for Footer Sidebar Area Section',
+		'id'          => 'footer-sidebar'
+	]);	
  ?>
