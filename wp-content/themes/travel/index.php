3ea2cc3764cba857a23e4bc6a31ca62a4205c05d
isrt09 <mazedur@gmail.com>
2020-12-23 00:24:20 +0600

    Built in function for dynamic

--- wp-content/themes/travel/index.php
+++ wp-content/themes/travel/index.php
@@ -3,12 +3,13 @@
 <head>
 	<meta charset="<?php bloginfo('charset') ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php bloginfo('title') ?> | <?php bloginfo('description'); ?></title>
+	<title><?php bloginfo('title') ?></title>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<?php wp_head(); ?>
 </head>
 <body class="<?php body_class(); ?>">
 	<h1><?php bloginfo('title') ?></h1>
+	<h2>Welcome to Our Community <span><?php bloginfo('name') ?></span></h2>
 	<?php wp_footer(); ?>
 </body>
 </html>
