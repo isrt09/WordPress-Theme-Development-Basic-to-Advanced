1a076a8da0cf24bfb0b8c42549eefb77bc4b81ab
isrt09 <mazedur@gmail.com>
2020-12-23 21:36:16 +0600

    Basic Theme Project Part 1

---
+++ wp-content/themes/Basic Project 1/functions.php
@@ -0,0 +1,9 @@
+<?php 
+	add_theme_support('menus');
+	add_theme_support('widgets');
+	add_theme_support('custom-logo');
+	add_theme_support('custom-header');
+	add_theme_support('custom-background');
+	add_theme_support('post-thumbnails');
+	add_theme_support('post-formats',['audio','video','gallery']);
+ ?>
---
+++ wp-content/themes/Basic Project 1/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style.css">
+      <title>Basic Project 1</title>
+   </head>
+   <body>
+      <section class="menu_section">
+         <div class="menu">
+            <div class="mid">
+               <div class="logo">
+                  <img src="images/logo.png" alt="">
+               </div>
+               <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Info</a></li>
+                  <li><a href="#">Contact</a></li>
+               </ul>
+            </div>
+         </div>
+      </section>
+      <section class="header_image">
+         <img src="images/woman-in-white-t-shirt-holding-smartphone-in-front-of-laptop-914931.jpg" alt="">
+      </section>
+      <section class="posts">
+         <div class="post">
+            <h2>Our Latest Post</h2>
+            <hr>
+            <div class="mid">
+               <div class="post1">
+                  <img src="images/SmartBuilding.jpg" alt="">
+                  <h3>Title Onee</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta sit quo possimus, iure ducimus. Ea dolor, molestias ab unde enim maxime quasi iste iure tempora quis laudantium voluptatibus labore aperiam eos voluptate neque nobis, laborum suscipit optio ad? Assumenda quam consequuntur perspiciatis recusandae atque autem doloremque nulla necessitatibus dolorum eaque commodi cum repellendus placeat dignissimos reiciendis veniam aperiam minus, doloribus ipsam possimus expedita molestiae. Ex corrupti distinctio ratione quaerat, atque. Quia, veritatis, voluptatem rerum iure incidunt eaque ipsa laudantium qui, accusantium delectus dolore aliquam id perferendis accusamus consequuntur! Ex, sint minima consequatur nulla delectus numquam, a ad sapiente quos fugiat.</p>
+               </div>
+               <div class="post1">
+                  <img src="images/SmartBuilding.jpg" alt="">
+                  <h3>Title Onee</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta sit quo possimus, iure ducimus. Ea dolor, molestias ab unde enim maxime quasi iste iure tempora quis laudantium voluptatibus labore aperiam eos voluptate neque nobis, laborum suscipit optio ad? Assumenda quam consequuntur perspiciatis recusandae atque autem doloremque nulla necessitatibus dolorum eaque commodi cum repellendus placeat dignissimos reiciendis veniam aperiam minus, doloribus ipsam possimus expedita molestiae. Ex corrupti distinctio ratione quaerat, atque. Quia, veritatis, voluptatem rerum iure incidunt eaque ipsa laudantium qui, accusantium delectus dolore aliquam id perferendis accusamus consequuntur! Ex, sint minima consequatur nulla delectus numquam, a ad sapiente quos fugiat.</p>
+               </div>
+               <div class="post1">
+                  <img src="images/SmartBuilding.jpg" alt="">
+                  <h3>Title Onee</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta sit quo possimus, iure ducimus. Ea dolor, molestias ab unde enim maxime quasi iste iure tempora quis laudantium voluptatibus labore aperiam eos voluptate neque nobis, laborum suscipit optio ad? Assumenda quam consequuntur perspiciatis recusandae atque autem doloremque nulla necessitatibus dolorum eaque commodi cum repellendus placeat dignissimos reiciendis veniam aperiam minus, doloribus ipsam possimus expedita molestiae. Ex corrupti distinctio ratione quaerat, atque. Quia, veritatis, voluptatem rerum iure incidunt eaque ipsa laudantium qui, accusantium delectus dolore aliquam id perferendis accusamus consequuntur! Ex, sint minima consequatur nulla delectus numquam, a ad sapiente quos fugiat.</p>
+               </div>
+            </div>
+         </div>
+      </section>
+   </body>
+</html>
