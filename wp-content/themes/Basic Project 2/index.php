04cba1fee9fd6beb2f3c3c49f3e3a6c753f822fa
isrt09 <mazedur@gmail.com>
2020-12-27 21:04:44 +0600

    Single Post Dynamic

--- wp-content/themes/Basic Project 2/index.php
+++ wp-content/themes/Basic Project 2/index.php
@@ -9,7 +9,8 @@
                      <div class="post-cont-b">
                         <h2><?php the_title(); ?></h2>
                         <p class="au">By : <?php the_author(); ?> | <?php the_time('g:i A'); ?></p>
-                        <p><?php the_content(); ?></p>
+                        <p><?php echo wp_trim_words(get_the_content(),10,true); ?></p><span><a href="<?php the_permalink(); ?>" class="read_more">Read More</a></span>
+
                      </div>
                   </div>
                   <?php endwhile; ?>                  
---
+++ wp-content/themes/Basic Project 2/single.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+      <div class="body-area">
+         <div class="mid">
+            <div class="left">
+               <div class="post-area">
+                  <?php while(have_posts()): the_post(); ?>
+                  <div class="post-box-b">
+                     <div class="post-img-b"><?php the_post_thumbnail();?></div>
+                     <div class="post-cont-b">
+                        <h2><?php the_title(); ?></h2>
+                        <p class="au">By : <?php the_author(); ?> | <?php the_time('g:i A'); ?></p>
+                        <p><?php the_content(); ?></p>
+                     </div>
+                  </div>
+                  <?php endwhile; ?>                  
+               </div>
+            </div>
+            <div class="right">
+              
+            </div>
+         </div>
+      </div>
+<?php get_footer(); ?>      
